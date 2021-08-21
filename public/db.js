2917769b045943315c50d39a17b165a097941c13
Eddie Yeung <eycs0317@gmail.com>
2021-08-21 14:22:06 -0700

    done - readme

--- public/db.js
+++ public/db.js
@@ -1,15 +1,55 @@
+  window.indexDB = window.indexDb || window.mozIndexedDB || window.webkitIndexedDb || window.msIndexedDB;
+  //make sure it is supported:
+  if (!window.indexedDB){
+    alert("Not Supported");
+  }
+let db;
 const request = indexedDB.open("budget", 1);
-
-
-request.onupgradeneeded = (event) => {
-  console.log("onupgradeneeded => executed");
-  const db = event.target.result;
-  const objectStore = db.createObjectStore("budget");
+request.onupgradeneeded = ({ target }) => {
+  let db = target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
 };
+request.onsuccess = ({ target }) => {
+  db = target.result;
 
-
-request.onsuccess = event => {
-  console.log(request.result.name);
-  const db = target.result;
-  const objectStore = db.createObjectStore("budget");
+  if (navigator.onLine) {
+    checkDatabase();
+  }
 };
+request.onerror = function(event) {
+  console.log("Woops! " + event.target.errorCode);
+};
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
+}
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => {
+        return response.json();
+      })
+      .then(() => {
+
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+        store.clear();
+      });
+    }
+  };
+}
+
+
+window.addEventListener("online", checkDatabase);
---
+++ readme.md
@@ -0,0 +1,37 @@
+# **Budget Tracker**
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  ## Description
+
+Budget Tracker ia a application that allow for user to track there budget either online/offline.
+
+
+
+  ## Table of Contents
+  * [Description](#description)
+  * [License](#license)
+  * [Deployed Link](#deployed-link)
+  * [Questions](#questions)
+  * [Sample Image](#sample-image)
+
+
+  ## Deployed Link
+
+  [Heroku Link]()
+
+  ## License
+  [MIT License](https://opensource.org/licenses/MIT)
+
+
+
+  ## Questions
+  If you have any questions: eycs0317@gmail.com
+
+  [My GitHub](https://github.com/eycs0317)
+
+  ## Sample Image
+
+
+  ![Sample1](./sampleimage/01.png)
+  ![Sample1](./sampleimage/02.png)
